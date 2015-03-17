80adc717c4e13839d61148abe19fa444c5b86334
James Dunwoody <jdunwoody@odecee.com.au>
2015-03-17 21:56:14 +1100

    Implemented touch

--- HexWorld/GameScene.swift
+++ HexWorld/GameScene.swift
@@ -141,24 +141,11 @@ class GameScene: SKScene {
         //        addChild(debugSteering.sprite)
     }
 
-    //    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-    //        /* Called when a touch begins */
-    //
-    //        for touch: AnyObject in touches {
-    //            let location = touch.locationInNode(self)
-    //            NSLog("Touched at %@", NSStringFromVector2D(location))
-    //
-    //            //            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-    //            //
-    //            //            sprite.xScale = 0.5
-    //            //            sprite.yScale = 0.5
-    //            //            sprite.position = location
-    //            //
-    //            //            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-    //            //
-    //            //            sprite.runAction(SKAction.repeatActionForever(action))
-    //            //
-    //            //            self.addChild(sprite)
-    //        }
-    //    }
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        for touch: AnyObject in touches {
+            let location = touch.locationInNode(self)
+
+            self.flock.lead.sprite.position = location
+        }
+    }
 }
